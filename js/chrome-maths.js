d71c5b19937a7b984fdba4b9a68081f26965164d
Andrew Shepherd <shepherd_a@hotmail.com>
2015-11-08 07:29:16 +1000

    Incorporating Angular-Route

--- js/chrome-maths.js
+++ js/chrome-maths.js
@@ -1,3 +1,10 @@
+var windowPathname = window.location.pathname;
+
+var baseText = "<base href='" + windowPathname + "' />";
+var baseElement = angular.element(baseText);
+angular.element('head').append(baseElement);
+
+
 var app = angular.module("chromeMathsApp", []);
 
 var controller = app.controller("mainController", ["$scope", function($scope) {
