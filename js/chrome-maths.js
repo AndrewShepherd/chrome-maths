9fa59d9414ad5a27d89d80e619786da03b2fd9eb
Andrew Shepherd <shepherd_a@hotmail.com>
2015-11-08 08:51:38 +1000

    Implemented the first routing template

--- js/chrome-maths.js
+++ js/chrome-maths.js
@@ -5,7 +5,14 @@ var baseElement = angular.element(baseText);
 angular.element('head').append(baseElement);
 
 
-var app = angular.module("chromeMathsApp", []);
+var app = angular.module("chromeMathsApp", ["ngRoute"]);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.when('/gcd', { templateUrl: 'templates/gcd.html'})
+		.otherwise({ templateUrl: 'templates/main.html'});
+	$locationProvider.html5Mode(true);
+}]);
+
 
 var controller = app.controller("mainController", ["$scope", function($scope) {
 	$scope.ApplicationTitle = "Chrome Maths";
