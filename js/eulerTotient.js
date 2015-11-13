834d918a2a46f2af8a57c24f477b3a810761e0cf
Andrew Shepherd <shepherd_a@hotmail.com>
2015-11-14 09:58:38 +1000

    Fixed performance and accuracy problems with totient function

--- js/eulerTotient.js
+++ js/eulerTotient.js
@@ -3,22 +3,24 @@ function eulerTotient(n, log) {
 		n = parseInt(n, 10);
 	}
 	log = log || (function () { });
-	var sieve = new Array(n + 1);
+
+	var maxI = Math.floor(Math.sqrt(n));
+	var sieve = new Array(maxI + 1);
 	var totient = n;
-	log('Value starts as ' + totient);
-	var maxI = Math.sqrt(n);
+
 	for (var i = 2; i <= maxI; ++i) {
 		if (!sieve[i]) {
 			if (n % i == 0) {
-				totient = n * (i - 1) / i;
-				log('' + n + ' is divisible by ' + i + ', so Totient now equals ' + n + '(' + (i - 1) + ')/' + i + ' = ' + totient);
+				var newTotient = totient * (i - 1) / i;
+				log('' + n + ' is divisible by ' + i + ', so Totient now equals ' + totient + '(' + (i - 1) + ')/' + i + ' = ' + newTotient);
+				totient = newTotient;
 				while (n % i == 0) {
 					n = n / i;
 				}
 				log('n now equals ' + n);
 				maxI = Math.sqrt(n);
 			}
-			for (var j = i * 2; j < sieve.length; j += i) {
+			for (var j = i * 2; j < maxI; j += i) {
 				sieve[j] = true;
 			}
 		}
