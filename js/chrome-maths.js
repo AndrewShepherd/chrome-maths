a70e51d556ad59bef89edba4a930b12ba17c849b
Andrew Shepherd <shepherd_a@hotmail.com>
2015-11-15 14:00:05 +1000

    Replaced window.setTeimout with

--- js/chrome-maths.js
+++ js/chrome-maths.js
@@ -131,7 +131,7 @@ app.controller('eulerTotientController', ['$scope', function ($scope) {
 	};
 }]);
 
-app.controller('chineseRemainderController', ['$scope', function ($scope) {
+app.controller('chineseRemainderController', ['$scope', '$timeout', function ($scope, $timeout) {
 	$scope.templateTitle = 'Chinese Remainder';
 
 	var generateId = (function () {
@@ -148,9 +148,9 @@ app.controller('chineseRemainderController', ['$scope', function ($scope) {
 			m: 0,
 			c: 1
 		});
-		window.setTimeout(function () {
+		$timeout(function () {
 			$(document.getElementById(id)).select().focus();
-		}, 100);
+		});
 	}
 	$scope.remove = function (pair) {
 		console.log("Remove pair");
